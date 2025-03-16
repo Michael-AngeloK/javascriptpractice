b8c265b2f37f727b7dde5aa2cf3d60238ed32c77
Michael-Angelo Karpowicz <michaelangelo.karpowicz@gmail.com>
2025-03-16 20:09:10 +0100

    Add more code for practice

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // 1st Method is prefered. used 99% of the time
-console.log(document.querySelector('#header'))
-console.log(document.querySelector('.headur'))
-console.log(document.querySelector('h1'))
+console.log(document.querySelector("#header"));
+console.log(document.querySelector(".header"));
+console.log(document.querySelector("h1"));
 
 // 2nd Method (Id use)
-console.log(document.getElementById('header'))
+console.log(document.getElementById("header"));
 
-// 3rd Method
+// Change target element / HTML / Content
+document.querySelector("h1").innerHTML = "Michael";
+document.querySelector("h1").innerHTML += "-Angelo";
+
+// Change CSS
+document.querySelector("h1").style.backgroundColor = "red";
+document.querySelector("h1").style.fontSize = "40px";
+
+function changeButtonToGreen() {
+  document.querySelector("button").style.backgroundColor = "green";
+}
+
+function toggleSidebar() {
+  document.querySelector('body').classList.toggle("open")
+}
